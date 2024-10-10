eb3a3dab12849df77d013a74c9fbe946ddd6f32f
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-10 15:41:14 -0400

    HTML

--- index.php
+++ index.php
@@ -8,3 +8,34 @@ vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori 
 <?php
 include __DIR__ .'/php-classes/movie.php'
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Movies</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+    <header class="text-center">
+        <h1>PHP Movies</h1>
+    </header>
+    <main>
+        <section class="container">
+            <div>
+                <div class="card" style="width: 18rem; heigth: 30rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"> <?= $message ?> </h5>
+                        <p class="card-text"></p>
+                        <h6 class="card-text"></h6>
+                    </div>
+                    </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
--- php-classes/movie.php
+++ php-classes/movie.php
@@ -1,4 +1,4 @@
 <?php
-
+$message = 'Ciao'
 ?>
 
